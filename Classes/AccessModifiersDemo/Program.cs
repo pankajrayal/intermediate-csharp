dc2f5bbf49f2b0e9123215c2764cc696f7b9555b
Pankaj Rayal <https://github.dxc.com/>
2021-06-13 19:51:37 +0530

    05-Access Modifiers - Private

---
+++ Classes/AccessModifiersDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AccessModifiersDemo
+{
+    public class Person
+    {
+        private DateTime _birthDate;
+
+        public void SetBirthDate(DateTime birthDate)
+        {
+            _birthDate = birthDate;
+        }
+
+        public DateTime GetBirthDate()
+        {
+            return _birthDate;
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthDate(new DateTime(1986, 4, 15));
+            Console.WriteLine(person.GetBirthDate());
+
+
+
+        }
+    }
+}
