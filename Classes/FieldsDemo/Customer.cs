a9c5c77be911677fd9862f7aa8295b0963338311
Pankaj Rayal <https://github.dxc.com/>
2021-06-13 19:37:31 +0530

    04-FieldsDemo

---
+++ Classes/FieldsDemo/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace FieldsDemo
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public readonly List<Order> Orders = new List<Order>();
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name) : this(id)
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            // Below field will error because the List<Order> is readonly.
+            //Orders = new List<Order>();
+        }
+    }
+}
