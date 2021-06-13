d070b3dea092c0ee8a3033c7f1afcdcc2faa4a59
Pankaj Rayal <https://github.dxc.com/>
2021-06-13 21:21:28 +0530

    06-PropertiesDemo

---
+++ Classes/PropertiesDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PropertiesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1986, 04, 15));
+            Console.WriteLine(person.Birthdate);
+        }
+    }
+}
