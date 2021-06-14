dc7484448af88af48007ddb2eb0a779cb6e8c19d
Pankaj Rayal <https://github.dxc.com/>
2021-06-14 11:30:36 +0530

    07-Indexers

---
+++ Classes/IndexersDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IndexersDemo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Pankaj";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
