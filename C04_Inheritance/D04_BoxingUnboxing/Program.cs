0d781dec257610599266bea5b00bf5e67ce47446
Pankaj Rayal <https://github.dxc.com/>
2021-06-14 15:24:43 +0530

    Boxing and Unboxing

---
+++ C04_Inheritance/D04_BoxingUnboxing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace D04_BoxingUnboxing
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Pankaj");
+            list.Add(DateTime.Now);
+
+            var number = (int)list[1];
+
+            var anotherList = new List<int>();
+            anotherList.Add(1);
+
+            var names = new List<string>();
+            names.Add("Pankaj");
+
+        }
+    }
+}
