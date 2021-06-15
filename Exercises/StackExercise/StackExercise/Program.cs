7620b1747016b5da89cab43e3b42e1da7cb521b2
Pankaj Rayal <https://github.dxc.com/>
2021-06-15 14:19:56 +0530

    Stack Exercise

---
+++ Exercises/StackExercise/StackExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StackExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            int ini = 1;
+            int max = 5;
+
+            for (int i = ini; i <= max; i++)
+                stack.Push(i);
+
+            for (int i = ini; i <= max; i++)
+                Console.WriteLine(stack.Pop());
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
---
+++ Exercises/StackExercise/StackExercise/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExercise
+{
+    public class Stack
+    {
+        private readonly List<object> _list = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("You can not add a null object to the stack.");
+
+            _list.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (_list.Count == 0)
+                throw new InvalidOperationException("There are no elements in the stack yet.");
+
+            var index = _list.Count - 1;
+            
+            var toReturn = _list[index];
+
+            _list.RemoveAt(index);
+
+            return (toReturn);
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+    }
+}
