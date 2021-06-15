46dff058a9b7ff9d8ba7029418cda8b3a9921aab
Pankaj Rayal <https://github.dxc.com/>
2021-06-15 14:05:38 +0530

    Stopwatch Exercise

---
+++ Exercises/StopWatch/StopWatch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+
+            for (var i = 0; i < 2; i++)
+            {
+                stopWatch.Start();
+                Thread.Sleep(1000);
+
+                stopWatch.Stop();
+
+                Console.WriteLine("Duration: " + stopWatch.GetInterval());
+                Console.WriteLine("Press ENTER to run the stopwatch one more time.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Exercises/StopWatch/StopWatch/StopWatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _running;
+
+        public void Start()
+        {
+            if (_running)
+            {
+                throw new InvalidOperationException("Stopwatch is already running.");
+            }
+            _startTime = DateTime.Now;
+            _running = true;
+        }
+
+        public void Stop()
+        {
+            if (!_running)
+            {
+                throw new InvalidOperationException("Stopwatch is not running.");
+            }
+            _endTime = DateTime.Now;
+            _running = false;
+        }
+
+        public TimeSpan GetInterval()
+        {
+            return _endTime - _startTime;
+        }
+    }
+}
