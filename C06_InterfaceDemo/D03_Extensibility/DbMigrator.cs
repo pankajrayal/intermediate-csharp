985b3687d64bf4962c5e02436ea7ead490dd48ea
Pankaj Rayal <https://github.dxc.com/>
2021-06-15 11:47:50 +0530

    Interfaces and Extensibility

---
+++ C06_InterfaceDemo/D03_Extensibility/DbMigrator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace D03_Extensibility
+{
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            //Console.WriteLine("Migration started at {0}", DateTime.Now);
+            //// Details of migrating the database
+            //Console.WriteLine("Migration finished at {0}", DateTime.Now);
+
+            _logger.LogInfo("Migration started at " + DateTime.Now);
+            //// Details of migrating the database
+            _logger.LogInfo("Migration finished at " + DateTime.Now);
+
+        }
+    }
+}
---
+++ C06_InterfaceDemo/D03_Extensibility/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace D03_Extensibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new FileLogger("C:\\data\\log.txt"));
+            dbMigrator.Migrate();
+        }
+    }
+}
