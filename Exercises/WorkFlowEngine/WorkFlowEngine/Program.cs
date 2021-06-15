d61646603d370a17399ef56f39a0fa5569dfb3ff
Pankaj Rayal <https://github.dxc.com/>
2021-06-15 14:42:12 +0530

    WorkflowEngine demo

---
+++ Exercises/WorkFlowEngine/WorkFlowEngine/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkFlowEngineDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workflow = new Workflow();
+            workflow.Add(new VideoUploader());
+            workflow.Add(new CallWebService());
+            workflow.Add(new SendMail());
+            workflow.Add(new ChangeStatus());
+
+            var engine = new WorkflowEngine();
+            engine.Run(workflow);
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface ITask
+    {
+        void Execute();
+    }
+
+    public interface IWorkflow
+    {
+        void Add(ITask task);
+        void Remove(ITask task);
+        IEnumerable<ITask> GetTasks();
+    }
+
+    class VideoUploader : ITask
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Uploading a video!");
+        }
+    }
+
+    class CallWebService : ITask
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Calling web service...");
+        }
+    }
+
+    class SendMail : ITask
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Sending an email...");
+        }
+    }
+
+    class ChangeStatus : ITask
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Statu changed.............");
+        }
+    }
+
+    public class WorkflowEngine
+    {
+        public void Run(IWorkflow workflow)
+        {
+            foreach(ITask I in workflow.GetTasks())
+            {
+                try
+                {
+                    I.Execute();
+                }
+                catch (Exception ex)
+                {
+                    // Logging
+                    // Terminate and persis the state of the workflow
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ Exercises/WorkFlowEngine/WorkFlowEngine/Workflow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace WorkFlowEngineDemo
+{
+    public class Workflow : IWorkflow
+    {
+        private readonly List<ITask> _tasks;
+        public Workflow()
+        {
+            _tasks = new List<ITask>();
+        }
+
+        public void Add(ITask task)
+        {
+            _tasks.Add(task);
+        }
+
+        public void Remove(ITask task)
+        {
+            _tasks.Remove(task);
+        }
+
+        public IEnumerable<ITask> GetTasks()
+        {
+            return _tasks;
+        }
+    }
+}
